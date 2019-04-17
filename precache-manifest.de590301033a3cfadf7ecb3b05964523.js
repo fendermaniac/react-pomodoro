2a79790903b01c353b43dfb54c6ad5bfa7f8f03e
Joe LoPresti <jlopresti87@gmail.com>
2019-04-16 22:18:31 -0400

    Updates

---
+++ precache-manifest.de590301033a3cfadf7ecb3b05964523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8762088bc9886dee5cbf",
+    "url": "/static/js/main.eb240b09.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "8762088bc9886dee5cbf",
+    "url": "/static/css/main.ef6f81e0.chunk.css"
+  },
+  {
+    "revision": "e6705b089ad0020825f375948110ad0a",
+    "url": "/index.html"
+  }
+];
